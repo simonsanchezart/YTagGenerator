cc8a48f484a97e29ef152185819db71d0294459e
simonsanchezart <simonsanchez.art@gmail.com>
2020-06-28 16:03:45 -0300

    Created whole software

---
+++ main.py
@@ -0,0 +1,77 @@
+from requests import get
+from pyinputplus import inputYesNo
+from sys import exit
+
+search_term = input("Insert term: ")
+api_key = open("api.json", "r").read()
+
+youtube_videos = []
+def search_videos():
+    url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q={search_term}&relevanceLanguage=en&type=video&key={api_key}"
+    r = get(url)
+    content = r.json()
+
+    videos = content["items"]
+    for video in videos:
+        video_id = video["id"]["videoId"]
+        youtube_videos.append(video_id)
+
+
+tags = []
+def get_tags():
+    for id in youtube_videos:
+        url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet&id={id}&key={api_key}"
+        r = get(url)
+        content = r.json()
+
+        try:
+            video_tags = content["items"][0]["snippet"]["tags"]
+        except KeyError:
+            continue
+        for tag in video_tags:
+            tags.append(tag)
+
+
+sorted_tags = []
+def sort_tags():
+    global sorted_tags
+    for i, tag in enumerate(tags):
+        tags[i] = tag.lower()
+    sorted_tags = sorted(tags, key=tags.count, reverse=True)
+    sorted_tags = list(dict.fromkeys(sorted_tags))
+
+
+final_tags = []
+def trim_tags():
+    global final_tags
+    char_count = 0
+    for tag in sorted_tags:
+        char_count += len(tag) + 1
+        if char_count < 500:
+            final_tags.append(tag)
+        else:
+            break
+
+def write_tags():
+    with open(f"{search_term}.txt", "w") as f:
+        for tag in final_tags:
+            try:
+                f.write(f"{tag},")
+            except UnicodeEncodeError:
+                continue
+
+def main():
+    search_videos()
+    get_tags()
+    sort_tags()
+    trim_tags()
+    write_tags()
+
+    another_search = inputYesNo(prompt="Would you like to search tags again? (y/n)\n")
+    if another_search == "yes":
+        main()
+    else:
+        exit()
+
+
+main()
